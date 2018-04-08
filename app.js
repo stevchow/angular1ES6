141b0b5e0e474218dbc377fd2bcaaaeb7c49c9df
stevchow <stephen_chow97@yahoo.co.id>
2018-04-08 18:51:07 +0700

    applying style-badpractice

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ myApp.controller("myCon", () => {
   let brolet = "asdasd";
   console.log(brolet);
   brolet = 12;
-  document.querySelector(".az").style.color = "red";
+  document.querySelector(".az").style = `
+    background: linear-gradient(310deg, #314755, #26a0da);
+    font-size:3rem;
+    display:inline-block;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    `;
   console.log({ a: "asd", c: 123213 });
   console.log(brolet);
 });

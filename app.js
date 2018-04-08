99325f45d4382084fe965b4a35a172145291fcb1
stevchow <stephen_chow97@yahoo.co.id>
2018-04-08 19:01:41 +0700

    separated css

--- app.js
+++ app.js
@@ -5,13 +5,7 @@ myApp.controller("myCon", () => {
   let brolet = "asdasd";
   console.log(brolet);
   brolet = 12;
-  document.querySelector(".az").style = `
-    background: linear-gradient(310deg, #314755, #26a0da);
-    font-size:3rem;
-    display:inline-block;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    `;
+  document.querySelector(".az").classList.add("gradient");
   console.log({ a: "asd", c: 123213 });
   console.log(brolet);
 });
